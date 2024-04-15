7f79495c25e68ef6524ab5b7b5b8250aa18ac501
hsw <244614297@qq.com>
2024-04-15 17:58:23 +0800

    master commit1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("hello git2 and ieda2");
         System.out.println("hello git3 and ieda3");
         System.out.println("hello git4 and idea4");
+        System.out.println("master hello");
     }
 }
