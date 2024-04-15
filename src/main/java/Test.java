0b0b58cc148813290905263488410df288aaf22e
hsw <244614297@qq.com>
2024-04-15 16:46:33 +0800

    second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * @author Tom Green
  * @create 2024-04-15 16:40
@@ -6,5 +8,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git and idea");
+        System.out.println("hello git2 and ieda2");
     }
 }
