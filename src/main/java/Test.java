79e3c4f0b303cc6c1519c41f2e2d761c85f2e490
hsw <244614297@qq.com>
2024-04-15 17:54:43 +0800

    hot-fix commit1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,6 +9,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git and idea");
         System.out.println("hello git2 and ieda2");
-        System.out.println("hello git2 and ieda2");
+        System.out.println("hello git3 and ieda3");
+        System.out.println("hello git4 and idea4");
     }
 }
