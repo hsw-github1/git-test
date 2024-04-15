94dc9cfefc4664f66f9e267f9f35c80752b03ab5
hsw <244614297@qq.com>
2024-04-15 16:43:18 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author Tom Green
+ * @create 2024-04-15 16:40
+ * @why LOVE
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git and idea");
+    }
+}
